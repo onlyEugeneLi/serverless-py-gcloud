f2ca78d98f6f4cdb1b97ced48bfcdfc27050df10
onlyEugeneLi <69563490+onlyEugeneLi@users.noreply.github.com>
2025-02-10 02:14:38 +0000

    Update test

--- src/tests.py
+++ src/tests.py
@@ -4,11 +4,20 @@ from src.main import app
 client = TestClient(app)
 
 def test_get_home_status():
-    path = "/"
-    # Python requests
-    # r = requests.get(path)
-    response = client.get(path)
-    status_code = response.status_code
-    content_type = response.headers["content-type"]
-    assert status_code == 200 # HTTP response - success reponse
-    assert content_type =="application/json"
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "text/html; charset=utf-8"
+    assert "<title>Input Form</title>" in response.text
+
+def test_submit():
+    response = client.post("/submit", data={"user_input": "Test input"})
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "text/html; charset=utf-8"
+    assert "Thank you for participating!" in response.text
+    assert "Test input" in response.text
+
+def test_submit_too_long_input():
+    long_input = "a" * 65  # 65 characters, which is too long
+    response = client.post("/submit", data={"user_input": long_input})
+    assert response.status_code == 422  # Bad Request, default error code from FastAPI
+    assert response.headers["content-type"] == "application/json"
